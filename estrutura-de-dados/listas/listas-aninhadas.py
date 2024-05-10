e4d1f0745a10ef177a709a2ccf2c0a7e8fb699a1
Stephanie <stephanie.exd1997@gmail.com>
2024-05-10 10:38:15 -0400

    exercicios listas

---
+++ estrutura-de-dados/listas/listas-aninhadas.py
@@ -0,0 +1,7 @@
+matriz = [
+    [0, 1, "a"],
+    [3, 3, "b"]
+]
+
+print(matriz[0])
+print(matriz[0][1])
---
+++ estrutura-de-dados/listas/listas.py
@@ -0,0 +1,18 @@
+     
+ #inicializando uma lista
+lista_alunos = [] # or list()
+
+#loop para iterar a lista que seja no máximo de tamanho 5
+while len(lista_alunos) < 5:
+    alunos = input()
+    lista_alunos.append(alunos)
+    
+print(lista_alunos)
+
+# retornando indices
+print(lista_alunos[1])
+    
+
+    
+
+
